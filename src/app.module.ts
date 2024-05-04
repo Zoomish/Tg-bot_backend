cd31ea89030a2d9a89811057fad0bbae26e24926
Zoom <egor147536987@gmail.com>
2024-05-04 19:58:02 +0300

    "Add PrismaService to providers in AppModule"
    This commit adds the PrismaService to the providers array in the AppModule of a NestJS application.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { BotService } from './bot/bot.service';
+import { PrismaService } from './prizma.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [BotService],
+  providers: [BotService, PrismaService],
 })
 export class AppModule {}

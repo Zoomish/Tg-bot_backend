07dab191c2ae398da922946bbb425f21dd163424
Zoom <egor147536987@gmail.com>
2024-05-04 19:54:26 +0300

    "Added method for creating Telegram bot instance in BotService"
    This commit adds a new method `botMesssage()` to the `BotService` class, which creates a new instance of the `TelegramBot` class

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -1,10 +1,17 @@
 import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import TelegramBot from 'node-telegram-bot-api';
 
 @Injectable()
 export class BotService implements OnModuleInit {
     constructor(private readonly prisma: PrismaClient) {}
   async onModuleInit() {
     await this.prisma.$connect();
+  }
+
+  async botMesssage(){
+    const bot = new TelegramBot(process.env.BOT_API_TOKEN, {
+        polling: true
+    })
   }
 }

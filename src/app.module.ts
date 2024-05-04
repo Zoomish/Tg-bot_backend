de6e012e4dee8a32861cfa7955a3aed3f9fddf3b
Zoom <egor147536987@gmail.com>
2024-05-04 20:13:53 +0300

    ESLint and Prettier config changes
    * Updated .eslintrc.js with rules for @typescript-eslint and prettier
    * Updated .prettierrc with trailingComma, tabWidth, semi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { BotService } from './bot/bot.service';
-import { PrismaService } from './prizma.service';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { BotService } from './bot/bot.service'
+import { PrismaService } from './prizma.service'
+import { AppService } from './app.service'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [BotService, PrismaService, AppService],
+    imports: [],
+    controllers: [AppController],
+    providers: [BotService, PrismaService, AppService],
 })
 export class AppModule {}
--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -1,23 +1,28 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
-import TelegramBot = require('node-telegram-bot-api');
-import { PrismaService } from 'src/prizma.service';
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common'
+import TelegramBot = require('node-telegram-bot-api')
+import { PrismaService } from 'src/prizma.service'
 
 @Injectable()
 export class BotService implements OnModuleInit {
-    constructor(private readonly prisma: PrismaService) { }
+    constructor(private readonly prisma: PrismaService) {}
     async onModuleInit() {
-        await this.botMesssage();
+        await this.botMesssage()
     }
 
     async botMesssage() {
         const bot = new TelegramBot(process.env.BOT_API_TOKEN, {
-            polling: true
+            polling: true,
         })
 
-        bot.on('new_chat_members', (msg) => bot.sendMessage(msg.chat.id, `Привет, ${msg.new_chat_members[0].first_name}!`))
+        bot.on('new_chat_members', (msg) =>
+            bot.sendMessage(
+                msg.chat.id,
+                `Привет, ${msg.new_chat_members[0].first_name}!`
+            )
+        )
 
         bot.on('message', async (msg) => {
-            console.log(msg);
+            console.log(msg)
         })
     }
 }

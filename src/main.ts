3c2bc88d12ed9f9e9e03a5367d6fa2579fb8cc7e
Zoom <egor147536987@gmail.com>
2024-05-05 15:15:43 +0300

    "Enable Prisma shutdown hooks and use process.env.PORT for server listening"
    This commit updates the main.ts file by:
    
    1. Importing the PrismaService from './prizma.service'.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { PrismaService } from './prizma.service'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
     app.enableCors()
-    await app.listen(3000)
+    const prismaSerbice = app.get(PrismaService)
+    await prismaSerbice.enableShutDownHooks(app)
+    await app.listen(process.env.PORT || 3000)
 }
 bootstrap()

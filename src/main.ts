42986429f303edfda9d11b2e7c4a5a9b563d1dcf
Zoom <egor147536987@gmail.com>
2024-05-05 19:56:57 +0300

    "Added periodic fetch of /reputations endpoint and enabled corresponding CORS"
    This commit includes the following changes:
    
    * In `app.controller.ts`, the `console.log('reputations')` line has been

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,14 @@ import { PrismaService } from './prizma.service'
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
     app.enableCors()
+    function aaa() {
+        setInterval(async () => {
+            console.log('reputations')
+            await fetch('https://tg-bot-backend-61qy.onrender.com/reputations')
+        }, 1000)
+    }
     const prismaSerbice = app.get(PrismaService)
     await prismaSerbice.enableShutDownHooks(app)
-    await app.listen(process.env.PORT)
+    await app.listen(process.env.PORT, () => aaa())
 }
 bootstrap()

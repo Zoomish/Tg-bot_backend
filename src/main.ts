3a8448a53e0f3c2953b043f7c9a5243abfa2c868
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-05-26 12:33:55 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
     function aaa() {
         setInterval(async () => {
             await fetch(
-                'https://tg-bot-backend-pfuu.onrender.com/projects/AAA'
+                'https://tg-bot-backend-5xns.onrender.com/projects/AAA'
             )
         }, 1000 * 60 * 14 + 1000 * 30)
     }
